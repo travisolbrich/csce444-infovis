1d6c01a94a22d948f77cc2c44b69991c596bcb7f
Travis Olbrich <travis.olbrich@gmail.com>
2015-11-17 16:24:23 -0600

    Standardize Mongo extraction scripts

--- scripts/mongo/get-users.js
+++ scripts/mongo/get-users.js
@@ -1,4 +1,4 @@
 //print(""id", "nickname"");
 db.getCollection('raw-data').find({}, {id: 1, nickname: 1}).forEach(function(user) {
-    print('"' + user.id + '","' + user.nickname + '",');
+    print(user.id + '||' + user.nickname);
 });
