1b0384307b436db21ef7886825876f2b3306ed37
Travis Olbrich <travis.olbrich@gmail.com>
2015-12-06 15:13:05 -0600

    Added API for basic extraction.

--- server/index.js
+++ server/index.js
@@ -1,22 +1,62 @@
 var mysql = require('mysql');
-var http = require('http');
 var express = require('express');
+var app = express();
 var sqlconn = require('./sqlconn.js');
 
-var server = http.createServer(function (request, response) {
+app.get('/words/frequency', function(req, res) {
 
     var connection = mysql.createConnection(sqlconn.conn());
-
     connection.connect();
 
-    connection.query('SELECT * from groups limit 1000', function (err, rows, fields) {
+    connection.query('SELECT count(*) as count, word FROM flickr.group_relations_word group by word;', function (err, rows, fields) {
         if (err) throw err;
 
-        response.writeHead(200, {"Content-Type": "application/json"});
-        response.end(JSON.stringify(rows));
+        res.send(JSON.stringify(rows));
     });
 
     connection.end();
 });
 
-server.listen(8080);
+app.get('/relations/word/:word', function(req, res) {
+
+    var connection = mysql.createConnection(sqlconn.conn());
+    connection.connect();
+
+    connection.query("select * from group_relations_word where word = '" + req.params.word + "'", function (err, rows, fields) {
+        if (err) throw err;
+
+        res.send(JSON.stringify(rows));
+    });
+
+    connection.end();
+});
+
+app.get('/group/:groupId', function(req, res) {
+
+    var connection = mysql.createConnection(sqlconn.conn());
+    connection.connect();
+
+    connection.query("select * from groups where groupId = '" + req.params.groupId + "'", function (err, rows, fields) {
+        if (err) throw err;
+
+        res.send(JSON.stringify(rows));
+    });
+
+    connection.end();
+});
+
+app.get('/group/:groupId/users', function(req, res) {
+
+    var connection = mysql.createConnection(sqlconn.conn());
+    connection.connect();
+
+    connection.query("select u.userId, nickname, isAdmin from users u join groups_users gu on gu.userId = u.userId where gu.groupId = '" + req.params.groupId + "'", function (err, rows, fields) {
+        if (err) throw err;
+
+        res.send(JSON.stringify(rows));
+    });
+
+    connection.end();
+});
+
+app.listen(80);
