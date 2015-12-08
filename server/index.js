62cd1cab1345e1860e38db182506cf3383457a1c
Travis Olbrich <travis.olbrich@gmail.com>
2015-12-08 16:52:56 -0600

    FIX

--- server/index.js
+++ server/index.js
@@ -35,16 +35,10 @@ app.get('/relations/word/:word', function(req, res) {
 
 app.get('/groups/word/:word/?minGroupSize', function(req, res) {
 
-    var query = "call groups_with_word_relation('"+req.params.word+"')"
-
-    if(req.params.minGroupSize != null) {
-        query = "select * from (call groups_with_word_relation('"+req.params.word+"')) g where trackedUserCount >" + req.params.minGroupSize;
-    }
-
     var connection = mysql.createConnection(sqlconn.conn());
     connection.connect();
 
-    connection.query(query, function (err, rows, fields) {
+    connection.query("call groups_with_word_relation('"+req.params.word+"',"+0+")", function (err, rows, fields) {
         if (err) throw err;
 
         res.send(JSON.stringify(rows[0]));
